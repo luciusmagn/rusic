77cb4532499c17902c65fa0381097766e92a88de
Lukáš Hozda <luk.hozda@gmail.com>
2017-11-06 16:22:42 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ extern crate rusting;
 extern crate async_command;
 
 use rusting::{Rust, e};
-use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::{clear, cursor, terminal_size, async_stdin, style};
 
 use std::io::Write;
 use std::ascii::AsciiExt;
@@ -29,18 +27,19 @@ fn main() {
 	let (mut buffer, mut temp_buffer, mut sic_buffer) = (String::new(), String::new(), String::new());
 	let (mut cursor_pos, mut history_pos) = (0, 0);
 	let mut sic = async_command::AsyncCommand::new(&mut ::std::process::Command::new("sic").arg("-n").arg("magnusi"));
-
+	let stdin = termion::async_stdin();
+	let mut keys = stdin.keys();
 	sic.run();
 
-	out!("{}{}{}", clear::All, cursor::Goto(1, 1), cursor::Hide);
+	out!("{}{}{}", termion::clear::All, termion::cursor::Goto(1, 1), termion::cursor::Hide);
 	loop {
-		out!("{}{}", clear::All, cursor::Goto(1,1));
-		let (col, line) = terminal_size().rust(e);
+		out!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
+		let (col, line) = termion::terminal_size().rust(e);
 		
-		if let Some(c) = async_stdin().keys().next() {
+		if let Some(c) = keys.next() {
 			match c.rust(e) {
-				Key::Ctrl('q') => break,
-				Key::Char('\n')=> {
+				termion::event::Key::Ctrl('q') => break,
+				termion::event::Key::Char('\n')=> {
 					history.push(buffer.clone());
 					history_pos = history.len() - 1;
 					sic.push(buffer.as_bytes());
@@ -48,12 +47,13 @@ fn main() {
 					buffer.clear();
 					cursor_pos = 0;
 				},
-				Key::Char(c) if c.is_ascii() => {
+				termion::event::Key::Char(c) if c.is_ascii() => {
 					buffer.insert(cursor_pos, c);
 					cursor_pos +=1;
 				},
-				Key::Backspace => {
+				termion::event::Key::Backspace => {
 					let len = buffer.len();
+					sic_buffer.push_str(&("{".to_string() + &cursor_pos.to_string() + "," + &len.to_string() + "}" + "\n\r"));
 					if cursor_pos == len && len != 0 {
 						buffer.truncate(len-1);
 						cursor_pos -= 1;
@@ -62,9 +62,9 @@ fn main() {
 						cursor_pos -= 1;
 					}
 				},
-				Key::Left  => if cursor_pos != 0 { cursor_pos -= 1; },
-				Key::Right => if cursor_pos != buffer.len() { cursor_pos += 1; },
-				Key::Up => if history_pos != 0 && history_pos != history.len() - 1 {
+				termion::event::Key::Left  => if cursor_pos != 0 { cursor_pos -= 1; },
+				termion::event::Key::Right => if cursor_pos != buffer.len() { cursor_pos += 1; },
+				termion::event::Key::Up => if history_pos != 0 && history_pos != history.len() - 1 {
 						buffer = history[history_pos].clone();
 						history_pos -= 1;
 						cursor_pos = buffer.len();
@@ -74,12 +74,12 @@ fn main() {
 						history_pos -= 1;
 						cursor_pos = buffer.len();
 					},
-				Key::Down => {
+				termion::event::Key::Down => {
 					if history_pos == history.len() -1 { buffer = temp_buffer.clone(); }
 					else { history_pos += 1; buffer = history[history_pos].clone(); }
 					cursor_pos = buffer.len();
 				},
-				_  => (),
+				e  => sic_buffer.push_str(&format!("{:?}", e)),
 			}
 		}
 
@@ -87,13 +87,13 @@ fn main() {
 			sic_buffer.push_str(&(line + "\n\r"));
 		}
 
-		out!("{}{}\n\n", cursor::Goto(1, 1), &sic_buffer);
-		out!("{}{}{}{}rusic 0.1{}\n\r", cursor::Goto(1, line-2), style::Bold, style::Invert, termion::color::Fg(termion::color::LightYellow), style::Reset);
-		out!("{}{}", cursor::Goto(1, line-1), (0..col).fold::<String, _>(String::new(), |mut a, _| { a.push('_'); a}));
-		out!("{}{}", cursor::Goto(1, line), &buffer);
-		out!("{}{}", cursor::Show, cursor::Goto((cursor_pos as u16 + 1) % col, line));
+		out!("{}{}\n\n", termion::cursor::Goto(1, 1), &sic_buffer);
+		out!("{}{}{}rusic 0.0.3{}\n\r", termion::cursor::Goto(1, line-2), termion::style::Invert, termion::color::Fg(termion::color::LightYellow), termion::style::Reset);
+		out!("{}{}", termion::cursor::Goto(1, line-1), (0..col).fold::<String, _>(String::new(), |mut a, _| { a.push('_'); a}));
+		out!("{}{}", termion::cursor::Goto(1, line), &buffer);
+		out!("{}{}", termion::cursor::Show, termion::cursor::Goto((cursor_pos as u16 + 1) % col, line));
 
 		::std::thread::sleep(::std::time::Duration::from_millis(33));
 	}
-	out!("{}", cursor::Show);
+	out!("{}", termion::cursor::Show);
 }
